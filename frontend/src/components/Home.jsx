d5d80ef2669aabe59c74e4be6e86f6bfb37fdd04
Shuva15 <shuvajitmahanta02@gmail.com>
2024-06-10 15:19:38 +0530

    initiated the backend

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import InputBox from './InputBox'
+
+const Home = () => {
+    
+  ruturn (
+    <div>
+        <InputBox />
+    </div>
+  )
+}
+
+export default Home
